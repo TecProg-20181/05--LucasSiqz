79cde9e04159bc437ba8a4fdc06d329f1d730e6b
Lucas Siqueira <lucassiqzro@gmail.com>
2018-06-22 20:55:50 -0300

    Add test for print tree function

--- diskspace/test.py
+++ diskspace/test.py
@@ -3,7 +3,8 @@ import diskspace
 import os
 import subprocess
 import re
-
+import sys
+import StringIO
 
 class TestDiskSpaceMethods(unittest.TestCase):
     def test_subprocess_check_output(self):
@@ -22,6 +23,23 @@ class TestDiskSpaceMethods(unittest.TestCase):
         self.assertEqual(diskspace.bytes_to_readable(blocksKb), '128.00Kb')
         self.assertEqual(diskspace.bytes_to_readable(blocksMb), '256.00Mb')
         self.assertEqual(diskspace.bytes_to_readable(blocksGb), '512.00Gb')
+
+    def testPrintTree(self):
+        path = '/home/Documentos'
+        total_size = 2
+        file_tree_node = {'print_size': '100.00Kb',
+                               'children': [], 'size': total_size}
+        file_tree = {path: file_tree_node}
+        largest_size = 4
+        captured = StringIO.StringIO()
+        sys.stdout = captured
+
+        diskspace.print_tree(file_tree, file_tree_node, path,
+                             largest_size, total_size)
+
+        result = "100.00Kb  100%  {}\n".format(path)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(result, captured.getvalue())
 
 
 if __name__ == '__main__':
